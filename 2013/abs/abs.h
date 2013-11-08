dfaf510e3b089df28b5abd6bfff9016ca70a79e2
Lasse Kortetjarvi <lasse.kortetjarvi@aalto.fi>
2013-11-08 21:32:45 +0200

    Abs-koodia alettu hahmottelemaan

---
+++ 2013/abs/abs.h
@@ -0,0 +1,24 @@
+#ifndef RCCAR_ABS_H
+#define RCCAR_ABS_H
+
+struct absData_t;
+
+typedef struct {
+    unsigned char slip;
+    unsigned char brakeForce;
+    int whlSpeed;
+    int whlAcc;
+    absData_t* otherSide;
+} absData_t;
+
+void setInitialSpeed(int speed);
+void calculateCurrentSpeed(int time);
+
+unsigned char getSilp(wheel);
+
+unsigned char isMuSplit(wheel);
+
+void setBrakeForce(wheel);
+
+
+#endif
