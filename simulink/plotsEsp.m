215ed139904cb4c8e91c6416410fcb8a0897361a
Lasse Kortetjarvi <lasse.kortetjarvi@aalto.fi>
2013-12-15 15:39:36 +0200

    Loppudokumentaatiota, liitteitä lisätty ja esp:stä kirjoitettu lisää

--- simulink/plotsEsp.m
+++ simulink/plotsEsp.m
@@ -2,8 +2,8 @@ figure
 
 subplot(2,1,1);
 
-plot(simData.Time,simData.Data(:,1),simData.Time,simData.Data(:,6), simData.Time, simData.Data(:,4)./100);
-legend('output', 'input', 'slipK');
+plot(simData.Time,simData.Data(:,1)*10,simData.Time,simData.Data(:,2));
+legend('Auton pyörähdysnopeus [rad/s]', 'Ratin kääntökulma [rad*10]');
 title(['ESP-simulaatio K:' num2str(pidData(1)) ' Ti:' num2str(pidData(2)) ' Td:' num2str(pidData(3))])
 
 
@@ -13,5 +13,5 @@ title(['ESP-simulaatio K:' num2str(pidData(1)) ' Ti:' num2str(pidData(2)) ' Td:'
 % legend('vehicle speed', 'wheel speed');
 
 subplot(2,1,2);
-plot(simData.Time, simData.Data(:,3).*100, simData.Time, simData.Data(:,4));
-legend('slip', 'wheel acceleration');
+plot(simData.Time, simData.Data(:,2), simData.Time, simData.Data(:,4));
+legend('Auton pyörähdysnopeus [rad/s]','Auton haluttu pyörähdysnopeus [rad/s]');
